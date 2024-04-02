a51bce1ad3916fb5b325391856e1709b8c673033
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-04-02 07:59:52 -0400

    scroll handling done

---
+++ src/test/java/scrollhandling/ScrollUp.java
@@ -0,0 +1,56 @@
+package scrollhandling;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class ScrollUp {
+	protected static String url = "https://www.amazon.com/";
+
+	Playwright playwright;
+	BrowserType browserType;
+	protected Browser browser;
+	protected BrowserContext context;
+	protected Page page;
+
+	@BeforeSuite
+	public void startChromeBrowser() {
+		playwright = Playwright.create();
+		browserType = playwright.chromium();
+		browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+		context = browser.newContext(new Browser.NewContextOptions());
+
+		page = browser.newPage();
+		System.out.println("**** Chrome Browser Version is : " + browser.version());
+	}
+
+	@BeforeClass
+	public void openUrl() throws InterruptedException {
+		page.navigate(url);
+		page.waitForLoadState();
+	}
+
+	@Test(priority = 0)
+	public void scrollUp() throws InterruptedException {
+		String scriptDown = "window.scrollTo(0, document.body.scrollHeight)";
+		page.evaluate(scriptDown);
+		Thread.sleep(5000);
+
+		String scriptUp = "window.scrollTo(0,0)";
+		page.evaluate(scriptUp);
+		Thread.sleep(3000);
+	}
+
+	@AfterSuite
+	public void closeChromeBrowser() {
+		page.close();
+		browser.close();
+		playwright.close();
+	}
+}
